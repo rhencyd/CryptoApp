942947a9a179b9be80066a19f26f63dacb643aea
rhencyd <rhencyd@gmail.com>
2023-06-04 14:48:50 -0500

    new update

--- CryptoApp/CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/CryptoApp/Core/Home/Views/HomeView.swift
@@ -8,13 +8,59 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            // Background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // Content layer
+            VStack {
+                homeHeader
+                                
+                Spacer(minLength: 0)
+            }
+            
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        NavigationStack {
+            HomeView()
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background {
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                }
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
--- CryptoApp/CryptoApp/Extensions/PreviewProvider.swift
+++ CryptoApp/CryptoApp/Extensions/PreviewProvider.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension PreviewProvider {
+    static var dev: DeveloperPreview {
+        return DeveloperPreview.instance
+    }
+}
+
+class DeveloperPreview {
+    static let instance = DeveloperPreview() // Singleton
+    private init() { }
+    
+    let coin = CoinModel(
+        id: "bitcoin",
+        symbol: "btc",
+        name: "Bitcoin",
+        image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+        currentPrice: 28773,
+        marketCap: 557488493384,
+        marketCapRank: 1,
+        fullyDilutedValuation: 604498717892,
+        totalVolume: 4702370493,
+        high24H: 29093,
+        low24H: 28773,
+        priceChange24H: -149.99957711444222,
+        priceChangePercentage24H: -0.51862,
+        marketCapChange24H: -2706935853.185669,
+        marketCapChangePercentage24H: -0.48321,
+        circulatingSupply: 19366887,
+        totalSupply: 21000000,
+        maxSupply: 21000000,
+        ath: 69045,
+        athChangePercentage: -58.30871,
+        athDate: "2021-11-10T14:24:11.849Z",
+        atl: 67.81,
+        atlChangePercentage: 42351.08058,
+        atlDate: "2013-07-06T00:00:00.000Z",
+        lastUpdated: "2023-05-07T23:23:48.856Z",
+        sparklineIn7D: SparklineIn7D(price: [
+            29739.909871176587,
+            29656.712525700674,
+        ]),
+        priceChangePercentage24HInCurrency: -0.5186181420032251,
+        currentHoldings: 1.5)
+}
